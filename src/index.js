253d5b3fb93e376d26fcd30a5cdd37087c583b1b
Martin Acosta <tinchoz49@gmail.com>
2021-08-05 10:20:09 -0300

    update codec

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
-function iterate (x, decode) {
-  if (typeof x !== 'object') {
-    if (decode && Object.prototype.toString.call(x) === '[object String]' && x.startsWith('base64:')) {
-      return Buffer.from(x.slice('base64:'.length), 'base64')
-    }
-    return x
-  }
+import varint from 'varint'
 
-  let k
-  let tmp
+function iterate (x, decode) {
   const type = Object.prototype.toString.call(x)
 
-  if (!decode && type === '[object Uint8Array]' && Buffer.isBuffer(x)) {
-    return 'base64:' + Buffer.from(x).toString('base64')
-  }
+  let tmp, k
 
   if (type === '[object Object]') {
     tmp = {}
@@ -30,16 +21,67 @@ function iterate (x, decode) {
     return tmp
   }
 
+  if (decode) {
+    if (type === '[object String]' && x.startsWith('base64:')) return Buffer.from(x.slice('base64:'.length), 'base64')
+  } else {
+    if (type === '[object Uint8Array]') return 'base64:' + Buffer.from(x).toString('base64')
+  }
+
   return x
 }
 
 export class BufferJSONCodec {
-  encode (obj) {
-    const str = JSON.stringify(iterate({ data: obj }))
-    return Buffer.from(str, 'utf-8')
+  constructor () {
+    this._lastObj = null
+    this._lastStr = null
+    this._lastLength = null
   }
 
-  decode (buf) {
+  encode (obj, buf, offset = 0) {
+    if (Buffer.isBuffer(obj)) {
+      buf = buf || Buffer.allocUnsafe(obj.length + 1)
+      varint.encode(1, buf, offset)
+      obj.copy(buf, offset + varint.encode.bytes)
+      return buf
+    }
+
+    let str
+    let strLength
+
+    if (this._lastObj === obj) {
+      str = this._lastStr
+      strLength = this._lastLength
+    } else {
+      str = JSON.stringify(iterate({ data: obj }))
+      strLength = Buffer.byteLength(str, 'utf8')
+    }
+
+    const bufferLength = strLength + 1
+
+    buf = buf || Buffer.allocUnsafe(bufferLength)
+    varint.encode(0, buf, offset)
+    buf.write(str, offset + varint.encode.bytes, strLength, 'utf8')
+    this._lastObj = null
+    this._lastStr = null
+    this._lastLength = null
+    return buf
+  }
+
+  decode (buf, start = 0, end = buf.length) {
+    start = start || 0
+    end = end || buf.length
+    buf = buf.slice(start, end)
+    const isBuffer = varint.decode(buf) === 1
+    buf = buf.slice(varint.decode.bytes)
+    if (isBuffer) return buf
     return iterate(JSON.parse(buf), true).data
+  }
+
+  encodingLength (obj) {
+    if (Buffer.isBuffer(obj)) return obj.length + 1
+    this._lastObj = obj
+    this._lastStr = JSON.stringify(iterate({ data: obj }))
+    this._lastLength = Buffer.byteLength(this._lastStr, 'utf8')
+    return this._lastLength + 1
   }
 }
--- tests/index.js
+++ tests/index.js
@@ -17,12 +17,32 @@ test('encode/decode', () => {
   }
 
   const codec = new BufferJSONCodec()
+
   const buf = codec.encode(obj)
   assert.instance(buf, Buffer)
-  assert.is(buf.length, 112)
+  assert.is(buf.length, 113)
 
   const newObj = codec.decode(buf)
   assert.equal(newObj, obj)
+
+  const buf2 = Buffer.from('test')
+  assert.equal(codec.decode(codec.encode(buf2)), buf2)
+})
+
+test('offset', () => {
+  const obj = {
+    string: 'test'
+  }
+
+  const codec = new BufferJSONCodec()
+
+  const length = codec.encodingLength(obj) + 4
+  const buf = Buffer.alloc(length)
+  buf.write('1234')
+  const encoded = codec.encode(obj, buf, 4)
+  const decoded = codec.decode(encoded, 4)
+  assert.is(buf.slice(0, 4).toString(), '1234')
+  assert.equal(decoded, obj)
 })
 
 test.run()
